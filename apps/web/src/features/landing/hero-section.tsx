513fa1f33986b5a2367c5cf2d54a053e9de8e60d
Nyoman Sunima <nyomansunima@gmail.com>
2025-12-15 12:13:37 +0800

    chore: update title

--- apps/web/src/features/landing/hero-section.tsx
+++ apps/web/src/features/landing/hero-section.tsx
@@ -4,14 +4,14 @@ export function HeroSection() {
       <span className="text-foreground/40 text-sm">Early stage</span>
 
       <h2 className="mt-10 text-pretty text-center font-medium text-5xl leading-tight tracking-tighter">
-        Create, manage & share
+        One tools for
         <br />
-        beautiful invoices
+        business transactions
       </h2>
 
       <p className="mt-6 text-pretty text-center text-foreground/60 leading-relaxed lg:w-4/12">
-        Free for business and high scalable invoices, bills, transactions
-        documents tools for pdf, and API.
+        Free & unlimited invoices, bills, purchase order and receipts
+        transactions tools for business.
       </p>
     </section>
   );
